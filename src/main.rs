d7adee7a7b760981086010cff07270c8938a8a04
justapig9020 <justapig9020@gmail.com>
2020-12-30 00:09:48 +0800

    Add cli arg "scale" for enlarge window

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use log::{error, debug};
+use clap::{App, Arg};
 
 #[macro_use]
 extern crate num_derive;
@@ -21,25 +22,50 @@ mod joypad;
 use vm::{Vm, WIDTH, HEIGHT};
 use joypad::{JoypadKey};
 
+const MAX_ENLARGE_SCALE: usize = 5;
+
+fn arg_check_range<T>(arg: &str, range: (T, T)) -> Result<T, String>
+    where T: Ord + std::str::FromStr + std::fmt::Display
+{
+    let (min, max) = range;
+    match arg.parse::<T>() {
+        Ok(n) if min <= n && n <= max => Ok(n),
+        Err(_) => Err(String::from("Please select an integer as argument")),
+        _ => Err(format!("Please select integer in range {} to {}", min , max)),
+    }
+}
+
 fn main() -> io::Result<()> {
     env_logger::init();
 
-    let args: Vec<String> = env::args().collect();
+    let prog = App::new("ruGameboy")
+                    .arg(Arg::with_name("scale")
+                            .help("Set the scale of enlarge in range [1-5]")
+                            .short("s")
+                            .long("scale")
+                            .default_value("1"))
+                    .arg(Arg::with_name("binary")
+                            .help("Set the binary file to run")
+                            .required(true))
+                    .get_matches();
 
-    if args.len() != 2 {
-        error!("Usage: ruGameboy < binary.gb>");
-        std::process::exit(1);
-    }
+    let bin_name = prog.value_of("binary").unwrap();
 
-    let mut file = File::open(&args[1])?;
+    let scale = prog.value_of("scale").unwrap();
+    let scale = arg_check_range(scale, (1, MAX_ENLARGE_SCALE)).unwrap_or_else(|e| {
+                    error!("scale: {}", e);
+                    std::process::exit(1);
+                });
+
+    let mut file = File::open(bin_name)?;
     let mut binary = Vec::new();
     file.read_to_end(&mut binary)?;
 
     let mut vm = Vm::new(binary);
     let mut window = Window::new(
         "rust Gameboy",
-        WIDTH,
-        HEIGHT,
+        WIDTH * scale,
+        HEIGHT * scale,
         WindowOptions::default(),
     ).unwrap_or_else(|e| { panic!("{}", e); });
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
